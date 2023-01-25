b865e11a0e3a7b23f8627422f224124ac9e7a37f
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-25 20:29:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,20 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [clicked, toggleClick] = useState(false);
+
   return (
-    <div id="main">
-      // Do not alter the main div
+   <div id="main">
+      {clicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
+
+      <button id="click" onClick={() => toggleClick((previous) => !previous)}>
+        Click me
+      </button>
     </div>
   );
 }
